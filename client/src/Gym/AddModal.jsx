75e0166e413aee1c351ef3d28bbc6ea26cd0e4c8
Clayton Chin <chinclayton98@gmail.com>
2022-08-09 09:41:11 -0700

    feat: added add exerise modal and passed down exercise name to tracker

---
+++ client/src/Gym/AddModal.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const AddModal = (props) => {
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('Barbell');
+  const [bodyPart, setBodyPart] = useState('Core');
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.setExercise([...props.exercise, {name: name, category: category, bodyPart: bodyPart}]);
+    props.setToggleModal(false);
+  };
+
+  return (
+    <div>
+      <label>Exercise Name</label>
+      <input onChange={(e) => {setName(e.target.value)}}></input>
+      <label>Body Part</label>
+      <select id="BodyPart" onChange={(e) => {setBodyPart(e.target.value)}}>
+        <option value="Core">Core</option>
+        <option value="Arms">Arms</option>
+        <option value="Back">Back</option>
+        <option value="Chest">Chest</option>
+        <option value="Legs">Legs</option>
+        <option value="Shoulders">Shoulders</option>
+        <option value="Full Body">Full Body</option>
+        <option value="Cardio">Cardio</option>
+      </select>
+      <label>Category</label>
+      <select id="Category" onChange={(e) => {setCategory(e.target.value)}}>
+        <option value="Barbell">Barbell</option>
+        <option value="Dumbell">Dumbell</option>
+        <option value="Machine">Machine</option>
+        <option value="Weighted Bodyweight">Weighted Bodyweight</option>
+        <option value="Assisted Bodyweight">Assisted Bodyweight</option>
+        <option value="Duration">Duration</option>
+      </select>
+      <div>Previous Exercises</div>
+      <button onClick={handleClick}>Add exercise</button>
+    </div>
+  )
+};
+
+export default AddModal;
--- client/src/Gym/Gym.jsx
+++ client/src/Gym/Gym.jsx
@@ -3,19 +3,33 @@ import axios from 'axios';
 import Badges from './Badges.jsx';
 import GymTitle from './GymTitle.jsx';
 import Tracker from './Tracker.jsx';
-import Add from './Add.jsx';
+import AddModal from './AddModal.jsx';
 import Cancel from './Cancel.jsx';
 
 const Gym = (props) => {
+  const [exercise, setExercise] = useState([]);
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const toggleAddModal = async (e) => {
+    e.preventDefault();
+    setToggleModal(true);
+  };
+
   return (
     <div>
+      {console.log('exercise', exercise)}
       <Badges/>
       <GymTitle/>
-      <Tracker/>
-      <Add/>
+      {exercise.map((e, i) => {
+        return <Tracker key={'tracker' + i} name={exercise[i]['name']}/>
+      })}
+      <button onClick={toggleAddModal}>add exercise</button>
+      <>
+        {toggleModal ? <AddModal exercise={exercise} setExercise={setExercise} setToggleModal={setToggleModal}/> : null}
+      </>
       <Cancel/>
     </div>
-  )
+  );
 };
 
 export default Gym;
