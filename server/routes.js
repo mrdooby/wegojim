eaf51c482650603f2b57b4ea0ade63206e64c214
Clayton Chin <chinclayton98@gmail.com>
2022-08-08 18:49:34 -0700

    feat: implemented MVC

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-
+const controller = require('./controller.js')
 app.use(express.json());
 const router = express.Router();
 
 router.get('/gym', (req, res) => {
-  res.send('hi')
+  controller.test()
+  .then((data) => {
+    res.send(data)
+  })
 });
 
 module.exports = router;
