31f9a172e4b451793c3299178cb525b4a6a9dd3d
Clayton Chin <chinclayton98@gmail.com>
2022-08-09 23:14:48 -0700

    feat: formatted navbar and added border

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -5,11 +5,13 @@ import styled from 'styled-components';
 const Navbar = (props) => {
   return (
     <NavbarContainer>
-      <h1>HOENN YOURSELF</h1>
-      <div onClick={(e) => {props.setView('Home Page')}}>Home Page</div>
-      <div onClick={(e) => {props.setView('Gym')}}>Gym</div>
-      <div onClick={(e) => {props.setView('Poffin House')}}>Poffin House</div>
-      <div onClick={(e) => {props.setView('Pokemon Center')}}>Pokemon Center</div>
+      <Title>HOENN YOURSELF</Title>
+      <WidgetContainers>
+        <Widget onClick={(e) => {props.setView('Home Page')}}>  Home Page</Widget>
+        <Widget onClick={(e) => {props.setView('Gym')}}>Gym</Widget>
+        <Widget onClick={(e) => {props.setView('Poffin House')}}>Poffin House</Widget>
+        <Widget onClick={(e) => {props.setView('Pokemon Center')}}>Pokemon Center</Widget>
+      </WidgetContainers>
     </NavbarContainer>
   )
 };
@@ -19,4 +21,21 @@ export default Navbar;
 const NavbarContainer = styled.div`
   display: flex;
   flex-direction: row;
-`
+  border: 3px solid;
+`;
+
+const Title = styled.p`
+  display: flex;
+  align-self: flex-start;
+  margin: 0px 0px 20px;
+`;
+
+const WidgetContainers = styled.div`
+  display: flex;
+  margin-left: auto;
+  margin-right: 0;
+`;
+
+const Widget = styled.div`
+  margin: 0 5px 20px;
+`;
